1745bb796bd7b41d501572ca140321047d7a1bab
tharinduMaheesha <IT19162706@my.sliit.lk>
2021-02-27 15:34:35 +0530

    Add items completed

--- PAF_LAB3/src/com/Item.java
+++ PAF_LAB3/src/com/Item.java
@@ -23,6 +23,48 @@ public class Item {
 
 	 return con;
 	}
+	
+	public String insertItem(String code, String name, String price, String desc) {
+		
+		Connection con = connect();
+		PreparedStatement preparedStmt;
+		String output = ""; 
+
+		
+		if (con == null)
+		{
+		return "Error connecting to the database";
+		}
+
+		String query = " insert into items (`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`)"
+				 + " values (?, ?, ?, ?, ?)";
+				try {
+					preparedStmt = con.prepareStatement(query);
+					
+					preparedStmt.setInt(1, 0);
+					preparedStmt.setString(2, code);
+					preparedStmt.setString(3, name);
+					preparedStmt.setDouble(4, Double.parseDouble(price));
+					preparedStmt.setString(5, desc);
+					
+					preparedStmt.execute();
+					con.close();
+					preparedStmt.close();
+					
+					output = "Inserted successfully"; 
+
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					 output = "Error while inserting";
+					 System.err.println(e.getMessage()); 	
+				}
+				
+				return output;
+				
+				
+
+
+	}
 
 
 }
